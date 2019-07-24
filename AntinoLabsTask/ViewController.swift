5f0e3c135ad2a2d82bdddb90b2bd3a6cddb52563
krishan226 <krishanpahadiya@gmail.com>
2019-07-24 18:02:55 +0530

    Initial Commit

---
+++ AntinoLabsTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AntinoLabsTask
+//
+//  Created by Krishan Kant on 24/07/19.
+//  Copyright © 2019 Krishan Kant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
